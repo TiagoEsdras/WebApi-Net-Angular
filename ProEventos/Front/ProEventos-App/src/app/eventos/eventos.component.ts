c7cf58b6df73d526183dc0b3b73538fb37548e34
Tiago Esdras <tiagoesdrasvieira@gmail.com>
2021-03-27 20:32:11 -0300

    Entendendo funcionamento dos componentes

--- ProEventos/Front/ProEventos-App/src/app/eventos/eventos.component.ts
+++ ProEventos/Front/ProEventos-App/src/app/eventos/eventos.component.ts
@@ -7,6 +7,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EventosComponent implements OnInit {
 
+  public eventos: any = [
+    {
+      Tema: 'Angular 11',
+      Local: 'Belo Horizonte'
+    },
+    {
+      Tema: '.NET 5',
+      Local: 'São Paulo'
+    },
+    {
+      Tema: 'Angular e suas novidades',
+      Local: 'Rio de Janeiro'
+    }
+  ]
+
   constructor() { }
 
   ngOnInit(): void {
