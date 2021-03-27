b842dc79a42325573151418280d54826eec522a7
Tiago Esdras <tiagoesdrasvieira@gmail.com>
2021-03-27 20:44:08 -0300

    Comecando implementar os dados via API

--- ProEventos/Front/ProEventos-App/src/app/eventos/eventos.component.ts
+++ ProEventos/Front/ProEventos-App/src/app/eventos/eventos.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,24 +8,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EventosComponent implements OnInit {
 
-  public eventos: any = [
-    {
-      Tema: 'Angular 11',
-      Local: 'Belo Horizonte'
-    },
-    {
-      Tema: '.NET 5',
-      Local: 'São Paulo'
-    },
-    {
-      Tema: 'Angular e suas novidades',
-      Local: 'Rio de Janeiro'
-    }
-  ]
+  public eventos: any;
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
+    this.getEventos();
+  }
+
+  public getEventos(): void {
+    this.http.get('https://localhost:5000/api/eventos').subscribe(
+      response => this.eventos = response,
+      error => console.log(error),
+    );
   }
 
 }
