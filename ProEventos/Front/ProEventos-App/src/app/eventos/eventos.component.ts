d481c0dd801e3321bb6a2b29256f77d38b1353e1
Tiago Esdras <tiagoesdrasvieira@gmail.com>
2021-03-28 15:21:47 -0300

    Implementando metodo de filtrar eventos por tema e local

--- ProEventos/Front/ProEventos-App/src/app/eventos/eventos.component.ts
+++ ProEventos/Front/ProEventos-App/src/app/eventos/eventos.component.ts
@@ -9,6 +9,28 @@ import { Component, OnInit } from '@angular/core';
 export class EventosComponent implements OnInit {
 
   public eventos: any = [];
+  public eventosFiltrados : any = [];
+  larguraImg : number = 150;
+  margemImg : number = 2;
+  exibirImg : boolean = false;
+  private _filtroLista : string = '';
+
+  public get filtroLista() : string {
+    return this._filtroLista;
+  }
+
+  public set filtroLista(value: string) {
+    this._filtroLista = value;
+    this.eventosFiltrados = this.filtroLista ? this.filtrarEventos(this.filtroLista) : this.eventos;
+  }
+
+  filtrarEventos(filtrarPor: string) : any {
+    filtrarPor = filtrarPor.toLocaleLowerCase();
+    return this.eventos.filter(
+      (evento : any) => evento.tema.toLocaleLowerCase().indexOf(filtrarPor) !== -1 ||
+      evento.local.toLocaleLowerCase().indexOf(filtrarPor) !== -1
+      )
+  }
 
   constructor(private http: HttpClient) { }
 
@@ -16,9 +38,16 @@ export class EventosComponent implements OnInit {
     this.getEventos();
   }
 
+  exibirEsconderImg() {
+    this.exibirImg = !this.exibirImg;
+  }
+
   public getEventos(): void {
     this.http.get('https://localhost:5001/api/eventos').subscribe(
-      response => this.eventos = response,
+      response => {
+        this.eventos = response;
+        this.eventosFiltrados = this.eventos;
+      },
       error => console.log(error),
     );
   }
