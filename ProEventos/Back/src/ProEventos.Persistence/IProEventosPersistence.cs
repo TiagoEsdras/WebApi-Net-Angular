0005e4d56da2f3750b04e2e9d0ca9ee1ff56db1c
Tiago Esdras <tiagoesdrasvieira@gmail.com>
2021-03-28 17:24:32 -0300

    Criando interface IProEventosPersistence

---
+++ ProEventos/Back/src/ProEventos.Persistence/IProEventosPersistence.cs
@@ -0,0 +1,25 @@
+using System.Threading.Tasks;
+using ProEventos.Domain;
+
+namespace ProEventos.Persistence
+{
+    public interface IProEventosPersistence
+    {
+        //Geral
+        void Add<T>(T entity) where T: class;
+        void Updade<T>(T entity) where T: class;
+        void Delete<T>(T entity) where T: class;
+        void DeleteRange<T>(T[] entity) where T: class;
+        Task<bool> SaveChangesAsync();
+
+        //Eventos
+        Task<Evento[]> GetAllEventosByTemaAsync(string tema, bool includePalestrantes);
+        Task<Evento[]> GetAllEventosAsync(bool includePalestrantes);
+        Task<Evento> GetEventoByIdAsync(int EventoId, bool includePalestrantes);
+
+        //Palestrantes
+        Task<Palestrante[]> GetAllPalestrantesByNomeAsync(string Nome, bool includeEventos);
+        Task<Palestrante[]> GetAllPalestrantesAsync(bool includeEventos);
+        Task<Palestrante> GetPalestranteByIdAsync(int PalestranteId, bool includeEventos);
+    }
+}
