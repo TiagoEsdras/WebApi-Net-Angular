36f96239123041dbbf86ae452e74c4f2b5290016
Tiago Esdras <tiagoesdrasvieira@gmail.com>
2021-03-27 22:03:04 -0300

    Finalizando instalações de pacotes css

--- ProEventos/Front/ProEventos-App/src/app/app.module.ts
+++ ProEventos/Front/ProEventos-App/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -17,7 +18,8 @@ import { PalestrantesComponent } from './palestrantes/palestrantes.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
