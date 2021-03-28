e112e33a0f12013fdcc5dd437e6f59a77a02e5e6
Tiago Esdras <tiagoesdrasvieira@gmail.com>
2021-03-27 21:25:52 -0300

    Configurando CORS startup.cs

--- ProEventos/Front/ProEventos-App/src/app/eventos/eventos.component.ts
+++ ProEventos/Front/ProEventos-App/src/app/eventos/eventos.component.ts
@@ -17,7 +17,7 @@ export class EventosComponent implements OnInit {
   }
 
   public getEventos(): void {
-    this.http.get('https://localhost:5000/api/eventos').subscribe(
+    this.http.get('https://localhost:5001/api/eventos').subscribe(
       response => this.eventos = response,
       error => console.log(error),
     );
