65a4ca3b1544712b01606a11425c13de9e953434
Tiago Esdras <tiagoesdrasvieira@gmail.com>
2022-01-07 18:15:41 -0300

    Criando a camada services front e refatorando getEventos para chamar o getEventos do service

--- ProEventos/Front/ProEventos-App/src/app/eventos/eventos.component.ts
+++ ProEventos/Front/ProEventos-App/src/app/eventos/eventos.component.ts
@@ -1,5 +1,5 @@
-import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { EventoService } from '../services/evento.service';
 
 @Component({
   selector: 'app-eventos',
@@ -32,7 +32,7 @@ export class EventosComponent implements OnInit {
       )
   }
 
-  constructor(private http: HttpClient) { }
+  constructor(private eventoService: EventoService) { }
 
   ngOnInit(): void {
     this.getEventos();
@@ -43,7 +43,7 @@ export class EventosComponent implements OnInit {
   }
 
   public getEventos(): void {
-    this.http.get('https://localhost:5001/api/eventos').subscribe(
+    this.eventoService.getEvento().subscribe(
       response => {
         this.eventos = response;
         this.eventosFiltrados = this.eventos;
--- ProEventos/Front/ProEventos-App/src/app/services/evento.service.ts
+++ ProEventos/Front/ProEventos-App/src/app/services/evento.service.ts
@@ -1,10 +1,15 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
   providedIn: 'root'
 })
 export class EventoService {
+  baseURL = 'https://localhost:5001/api/eventos';
+  constructor(private http: HttpClient) { }
 
-constructor() { }
+  getEvento() {
+    return this.http.get(this.baseURL)
+  }
 
 }
