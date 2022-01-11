5fdae7cc7226342eada30e7427fe4a7f59c7a4ab
Tiago Esdras <tiagoesdrasvieira@gmail.com>
2022-01-11 19:48:45 -0300

    Angular Rotas Parte 01

--- ProEventos/Front/ProEventos-App/src/app/app-routing.module.ts
+++ ProEventos/Front/ProEventos-App/src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ContatosComponent } from './components/contatos/contatos.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
 
-const routes: Routes = [];
+import { EventosComponent } from './components/eventos/eventos.component';
+import { PalestrantesComponent } from './components/palestrantes/palestrantes.component';
+import { PerfilComponent } from './components/perfil/perfil.component';
+
+const routes: Routes = [
+  { path: 'eventos', component: EventosComponent },
+  { path: 'palestrantes', component: PalestrantesComponent },
+  { path: 'perfil', component: PerfilComponent },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'contatos', component: ContatosComponent },
+  { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
+  { path: '**', redirectTo: 'dashboard', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
