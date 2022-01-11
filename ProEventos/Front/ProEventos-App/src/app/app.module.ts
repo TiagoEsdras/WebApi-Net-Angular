9b3d02d8ce38fe674da1dc2387b2960b1dee709e
Tiago Esdras <tiagoesdrasvieira@gmail.com>
2022-01-11 20:21:02 -0300

    Angular Rotas - Parte 02

--- ProEventos/Front/ProEventos-App/src/app/app.module.ts
+++ ProEventos/Front/ProEventos-App/src/app/app.module.ts
@@ -18,7 +18,7 @@ import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { PerfilComponent } from './components/perfil/perfil.component';
 import { EventosComponent } from './components/eventos/eventos.component';
 import { PalestrantesComponent } from './components/palestrantes/palestrantes.component';
-import { NavComponent } from './nav/nav.component';
+import { NavComponent } from './shared/nav/nav.component';
 import { TituloComponent } from './shared/titulo/titulo.component';
 
 import { EventoService } from './services/evento.service';
