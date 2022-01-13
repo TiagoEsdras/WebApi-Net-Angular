8b1c6be056f0ab41b8962e74e6fa87e74697c1b1
Tiago Esdras <tiagoesdrasvieira@gmail.com>
2022-01-13 19:47:01 -0300

    paths tsconfig.json

--- ProEventos/Front/ProEventos-App/src/app/components/eventos/evento-lista/evento-lista.component.ts
+++ ProEventos/Front/ProEventos-App/src/app/components/eventos/evento-lista/evento-lista.component.ts
@@ -3,8 +3,8 @@ import { Router } from '@angular/router';
 import { BsModalRef, BsModalService } from 'ngx-bootstrap/modal';
 import { NgxSpinnerService } from 'ngx-spinner';
 import { ToastrService } from 'ngx-toastr';
-import { Evento } from '../../../models/Evento';
-import { EventoService } from '../../../services/evento.service';
+import { Evento } from '@app/models/Evento';
+import { EventoService } from '@app/services/evento.service';
 
 
 @Component({
