2a020cac5c9f403418e6dae5fa68630fe797466a
Tiago Esdras <tiagoesdrasvieira@gmail.com>
2022-01-14 00:30:33 -0300

    Validação forms perfil

--- ProEventos/Front/ProEventos-App/src/app/components/user/perfil/perfil.component.ts
+++ ProEventos/Front/ProEventos-App/src/app/components/user/perfil/perfil.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AbstractControlOptions, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ValidatorField } from '@app/helpers/ValidatorField';
 
 @Component({
   selector: 'app-perfil',
@@ -7,9 +9,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PerfilComponent implements OnInit {
 
-  constructor() { }
+  form: FormGroup;
 
-  ngOnInit() {
+  get f(): any {
+    return this.form.controls;
   }
 
+  constructor(private fb: FormBuilder) { }
+
+  ngOnInit(): void {
+    this.validacoes();
+  }
+
+  public validacoes(): void {
+    const formOptions: AbstractControlOptions = {
+      validators: ValidatorField.MustMatch('senha', 'confirmarSenha')
+    };
+
+    this.form = this.fb.group({
+      titulo: ['', Validators.required],
+      nome: ['', [Validators.required, Validators.minLength(3)]],
+      sobrenome: ['', [Validators.required, Validators.minLength(3)]],
+      email: ['', [Validators.required, Validators.email]],
+      telefone: ['', Validators.required],
+      funcao: ['', Validators.required],
+      descricao: ['', [Validators.required, Validators.minLength(30), Validators.maxLength(150)]],
+      senha: ['', [Validators.required, Validators.minLength(8)]],
+      confirmarSenha: ['', Validators.required]
+    }, formOptions);
+  }
+
+  public resetForm(): void {
+    this.form.reset();
+  }
 }
