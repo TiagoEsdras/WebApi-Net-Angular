31ccad49abedbc474ea63ef1d7fea79cdb865afb
Tiago Esdras <tiagoesdrasvieira@gmail.com>
2022-01-13 21:45:04 -0300

    Reactive forms - parte 02

--- ProEventos/Front/ProEventos-App/src/app/components/eventos/evento-detalhe/evento-detalhe.component.ts
+++ ProEventos/Front/ProEventos-App/src/app/components/eventos/evento-detalhe/evento-detalhe.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Form, FormControl, FormGroup } from '@angular/forms';
+import { Form, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-evento-detalhe',
@@ -16,13 +16,19 @@ export class EventoDetalheComponent implements OnInit {
 
   public validacoes(): void {
     this.form = new FormGroup({
-      local: new FormControl(),
-      dataEvento: new FormControl(),
-      tema: new FormControl(),
-      qtdPessoas: new FormControl(),
-      imagemURL: new FormControl(),
-      telefone: new FormControl(),
-      email: new FormControl(),
+      tema: new FormControl('',
+        [Validators.required, Validators.minLength(4), Validators.maxLength(50)]
+      ),
+      local: new FormControl('', Validators.required),
+      dataEvento: new FormControl('', Validators.required),
+      qtdPessoas: new FormControl('',
+        [Validators.required, Validators.max(120000)]
+      ),
+      telefone: new FormControl('', Validators.required),
+      email: new FormControl('',
+        [Validators.required, Validators.email]
+      ),
+      imagemURL: new FormControl('', Validators.required),
     });
   }
 
