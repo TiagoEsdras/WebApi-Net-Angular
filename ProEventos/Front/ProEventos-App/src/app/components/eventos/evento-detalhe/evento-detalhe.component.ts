03e6eea7f45ab20f436dd4eb7e6f0685cfc61f83
Tiago Esdras <tiagoesdrasvieira@gmail.com>
2022-01-24 21:34:18 -0300

    Ajustando data, spinner e toastr evento-detalhe

--- ProEventos/Front/ProEventos-App/src/app/components/eventos/evento-detalhe/evento-detalhe.component.ts
+++ ProEventos/Front/ProEventos-App/src/app/components/eventos/evento-detalhe/evento-detalhe.component.ts
@@ -1,6 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
 import { BsLocaleService } from 'ngx-bootstrap/datepicker';
+import { NgxSpinnerService } from 'ngx-spinner';
+import { ToastrService } from 'ngx-toastr';
+import { EventoService } from '@app/services/evento.service';
+import { Evento } from '@app/models/Evento';
 
 @Component({
   selector: 'app-evento-detalhe',
@@ -9,6 +14,7 @@ import { BsLocaleService } from 'ngx-bootstrap/datepicker';
 })
 export class EventoDetalheComponent implements OnInit {
 
+  evento: Evento;
   form: FormGroup;
 
   get f(): any {
@@ -26,13 +32,18 @@ export class EventoDetalheComponent implements OnInit {
 
   constructor(
     private fb: FormBuilder,
-    private localeService: BsLocaleService
+    private localeService: BsLocaleService,
+    private router: ActivatedRoute,
+    private eventoService: EventoService,
+    private spinner: NgxSpinnerService,
+    private toastr: ToastrService
     ) {
       this.localeService.use('pt-br');
     }
 
   ngOnInit(): void {
     this.validacoes();
+    this.carregarEvento();
   }
 
   public validacoes(): void {
@@ -43,7 +54,7 @@ export class EventoDetalheComponent implements OnInit {
       qtdPessoas: ['', [Validators.required, Validators.max(120000)]],
       telefone: ['', Validators.required],
       email: ['', [Validators.required, Validators.email]],
-      imagemURL: ['', Validators.required],
+      imageURL: ['', Validators.required],
     });
   }
 
@@ -53,5 +64,27 @@ export class EventoDetalheComponent implements OnInit {
 
   public validateCss(campo: FormControl): any {
     return {'is-invalid': campo.errors && campo.touched};
+  }
+
+  public carregarEvento(): void{
+    const eventoIdParam = this.router.snapshot.paramMap.get('id');
+
+    if(eventoIdParam !== null) {
+      this.spinner.show();
+      this.eventoService.getEventoById(+eventoIdParam).subscribe({
+        next: (evento: Evento) => {
+          this.evento = {...evento};
+          this.form.patchValue(this.evento);
+        },
+        error: (error: any) => {
+          this.spinner.hide();
+          this.toastr.error('Erro ao tentar carregar o evento!')
+          console.error(error);
+        },
+        complete: () => {
+          this.spinner.hide();
+        }
+      });
+    }
   }
 }
