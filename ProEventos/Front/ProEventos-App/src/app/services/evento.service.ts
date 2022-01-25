4c917b7ec2dad2d3606bd7ad4888f9cc75df13dc
Tiago Esdras <tiagoesdrasvieira@gmail.com>
2022-01-25 09:18:35 -0300

    post, put e delete eventoService front

--- ProEventos/Front/ProEventos-App/src/app/services/evento.service.ts
+++ ProEventos/Front/ProEventos-App/src/app/services/evento.service.ts
@@ -19,4 +19,16 @@ export class EventoService {
   public getEventoById(id : number): Observable<Evento> {
     return this.http.get<Evento>(`${this.baseURL}/${id}`);
   }
+
+  public postEvento(evento: Evento): Observable<Evento> {
+    return this.http.post<Evento>(this.baseURL, evento);
+  }
+
+  public putEvento(id : number, evento: Evento): Observable<Evento> {
+    return this.http.put<Evento>(`${this.baseURL}/${id}`, evento);
+  }
+
+  public deleteEvento(id : number): Observable<string> {
+    return this.http.delete<string>(`${this.baseURL}/${id}`);
+  }
 }
