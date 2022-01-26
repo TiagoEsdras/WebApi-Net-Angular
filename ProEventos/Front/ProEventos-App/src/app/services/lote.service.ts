42912c8b2a993e98e342a4f4cc7690359b383a84
Tiago Esdras <tiagoesdrasvieira@gmail.com>
2022-01-26 18:43:28 -0300

    LoteService front

---
+++ ProEventos/Front/ProEventos-App/src/app/services/lote.service.ts
@@ -0,0 +1,26 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Lote } from '@app/models/Lote';
+import { Observable } from 'rxjs';
+import { take } from 'rxjs/operators';
+
+@Injectable()
+export class LoteService {
+  baseURL = 'https://localhost:5001/api/lotes';
+  constructor(private http: HttpClient) { }
+
+  public getLotesByEventoId(eventoId: number): Observable<Lote[]> {
+    return this.http.get<Lote[]>(`${this.baseURL}/${eventoId}`)
+      .pipe(take(1));
+  }
+
+  public saveLotes(eventoId: number, lotes: Lote[]): Observable<Lote[]> {
+    return this.http.put<Lote[]>(`${this.baseURL}/${eventoId}`, lotes)
+      .pipe(take(1));
+  }
+
+  public deleteLote(eventoId : number, loteId: number): Observable<boolean> {
+    return this.http.delete<boolean>(`${this.baseURL}/${eventoId}/${loteId}`)
+      .pipe(take(1));
+  }
+}
