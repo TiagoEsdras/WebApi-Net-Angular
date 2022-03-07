33cdbe6dca18386cd0d8082ccf7e21672d24192e
Tiago Esdras <tiagoesdrasvieira@gmail.com>
2022-03-07 20:08:59 -0300

    Criando IAccountService e dtos

---
+++ ProEventos/Back/src/ProEventos.Application/Contratos/IAccountService.cs
@@ -0,0 +1,15 @@
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using ProEventos.Application.Dtos;
+
+namespace ProEventos.Application.Contratos
+{
+    public interface IAccountService
+    {   
+        Task<bool> UserExist(string username);
+        Task<UserUpdateDto> GetUserByUsernameAsync(string username);
+        Task<SignInResult> CheckUserPasswordAsync(UserUpdateDto userUpdadeDto, string password);
+        Task<UserDto> CrateAccountAsync(UserDto userDto);
+        Task<UserUpdateDto> UpdateAccount(UserUpdateDto userUpdateDto);
+    }
+}
---
+++ ProEventos/Back/src/ProEventos.Application/Dtos/UserDto.cs
@@ -0,0 +1,11 @@
+namespace ProEventos.Application.Dtos
+{
+    public class UserDto
+    {
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string PrimeiroNome { get; set; }
+        public string UltimoNome { get; set; }
+    }
+}
