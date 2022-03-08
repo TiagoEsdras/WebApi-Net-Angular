409029e981054ad813c124b7f18368acc5669a48
Tiago Esdras <tiagoesdrasvieira@gmail.com>
2022-03-07 22:13:03 -0300

    Ajustes Startup e configuracoes

--- ProEventos/Back/src/ProEventos.API/Startup.cs
+++ ProEventos/Back/src/ProEventos.API/Startup.cs
@@ -15,6 +15,7 @@ using System;
 using Microsoft.Extensions.FileProviders;
 using System.IO;
 using Microsoft.AspNetCore.Http;
+using System.Text.Json.Serialization;
 
 namespace ProEventos.API
 {
@@ -34,18 +35,25 @@ namespace ProEventos.API
                 context => context.UseSqlite(Configuration.GetConnectionString("Default"))
             );
             services.AddControllers()
-                .AddNewtonsoftJson(x => x.SerializerSettings.ReferenceLoopHandling =
+                .AddJsonOptions(
+                    options => options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter())
+                )
+                .AddNewtonsoftJson(
+                    options => options.SerializerSettings.ReferenceLoopHandling =
                     Newtonsoft.Json.ReferenceLoopHandling.Ignore
-            );
+                );
 
             services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
             services.AddScoped<IEventoService, EventoService>();
             services.AddScoped<ILoteService, LoteService>();
+            services.AddScoped<ITokenService, TokenService>();
+            services.AddScoped<IAccountService, AccountService>();
             
             services.AddScoped<IGeralPersist, GeralPersist>();
             services.AddScoped<IEventoPersist, EventoPersist>();
-            services.AddScoped<ILotePersist, LotePersist>();
+            services.AddScoped<ILotePersist, LotePersist>();            
+            services.AddScoped<IUserPersist, UserPersist>();
 
             services.AddCors();
             services.AddSwaggerGen(c =>
