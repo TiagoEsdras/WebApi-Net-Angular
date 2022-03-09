efd6afff465bb33c3312b3606ddd43355d7d3a9f
Tiago Esdras <tiagoesdrasvieira@gmail.com>
2022-03-09 20:48:19 -0300

    Implementando login

--- ProEventos/Back/src/ProEventos.API/Controllers/AccountController.cs
+++ ProEventos/Back/src/ProEventos.API/Controllers/AccountController.cs
@@ -45,18 +45,45 @@ namespace ProEventos.API.Controllers
         
         [HttpPost("Register")]
         [AllowAnonymous]
-        public async Task<IActionResult> Register(UserDto userDto)
+        public async Task<IActionResult> Register(UserDto user)
         {
             try
             {
-                if(await _accountService.UserExist(userDto.UserName)) {
+                if(await _accountService.UserExist(user.UserName)) {
                     return BadRequest("Usuário já existe!");
                 }
 
-                var userCreated = await _accountService.CrateAccountAsync(userDto); 
+                var userCreated = await _accountService.CrateAccountAsync(user); 
                     
                 return userCreated != null ? Ok(userCreated)
                     : BadRequest("Usuário não criado.Tente novamente mais tarde");
+            }
+            catch (Exception ex)
+            {
+                return this.StatusCode(StatusCodes.Status500InternalServerError,
+                $"Erro ao tentar recuperar usuário. Erro: {ex.Message}");
+            }
+        }
+
+        [HttpPost("Login")]
+        [AllowAnonymous]
+        public async Task<IActionResult> Login(UserLoginDto userLogin)
+        {
+            try
+            {
+                var user = await _accountService.GetUserByUserNameAsync(userLogin.Username);
+
+                if(user == null) 
+                    return BadRequest("Usuário inválido");
+                
+                var result = await _accountService.CheckUserPasswordAsync(user, userLogin.Password);
+
+                return !result.Succeeded ? Unauthorized("Senha incorreta")
+                    : Ok(new {
+                        userName = user.Username,
+                        primeiroNome = user.PrimeiroNome,
+                        token = _tokenService.CreateToken(user).Result
+                    });
             }
             catch (Exception ex)
             {
