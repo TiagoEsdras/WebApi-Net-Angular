0aeedae83e70fa969b025cc54fc0a57733b193ca
Tiago Esdras <tiagoesdrasvieira@gmail.com>
2025-01-13 13:40:36 -0300

    add register funcionality

--- ProEventos/Front/ProEventos-App/src/app/components/user/cadastro/cadastro.component.ts
+++ ProEventos/Front/ProEventos-App/src/app/components/user/cadastro/cadastro.component.ts
@@ -1,21 +1,36 @@
 import { Component, OnInit } from '@angular/core';
-import { AbstractControlOptions, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import {
+  AbstractControlOptions,
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
+import { Router } from '@angular/router';
 import { ValidatorField } from '@app/helpers/ValidatorField';
+import { User } from '@app/models/Identity/User';
+import { AccountService } from '@app/services/account.service';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-cadastro',
   templateUrl: './cadastro.component.html',
-  styleUrls: ['./cadastro.component.scss']
+  styleUrls: ['./cadastro.component.scss'],
 })
 export class CadastroComponent implements OnInit {
-
+  user = {} as User;
   form: FormGroup;
 
   get f(): any {
     return this.form.controls;
   }
 
-  constructor(private fb: FormBuilder) { }
+  constructor(
+    private fb: FormBuilder,
+    private accountService: AccountService,
+    private router: Router,
+    private toastr: ToastrService
+  ) {}
 
   ngOnInit(): void {
     this.validacoes();
@@ -23,21 +38,32 @@ export class CadastroComponent implements OnInit {
 
   public validacoes(): void {
     const formOptions: AbstractControlOptions = {
-      validators: ValidatorField.MustMatch('senha', 'confirmarSenha')
+      validators: ValidatorField.MustMatch('password', 'confirmarPassword'),
     };
 
-    this.form = this.fb.group({
-      nome: ['', [Validators.required, Validators.minLength(3)]],
-      sobrenome: ['', [Validators.required, Validators.minLength(3)]],
-      email: ['', [Validators.required, Validators.email]],
-      usuario: ['', Validators.required],
-      senha: ['', [Validators.required, Validators.minLength(8)]],
-      confirmarSenha: ['', Validators.required],
-      termo: [null, Validators.pattern('true')]
-    }, formOptions);
+    this.form = this.fb.group(
+      {
+        primeiroNome: ['', [Validators.required, Validators.minLength(3)]],
+        ultimoNome: ['', [Validators.required, Validators.minLength(3)]],
+        email: ['', [Validators.required, Validators.email]],
+        userName: ['', Validators.required],
+        password: ['', [Validators.required, Validators.minLength(8)]],
+        confirmarPassword: ['', Validators.required],
+        termo: [null, Validators.pattern('true')],
+      },
+      formOptions
+    );
   }
 
   public validateCss(campo: FormControl): any {
-    return {'is-invalid': campo.errors && campo.touched};
+    return { 'is-invalid': campo.errors && campo.touched };
+  }
+
+  public register(): void {
+    this.user = { ...this.form.value };
+    this.accountService.register(this.user).subscribe(
+      () => this.router.navigateByUrl('/user/login'),
+      (err: any) => this.toastr.error(err.error)
+    );
   }
 }
