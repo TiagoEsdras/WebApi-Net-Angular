7f691bc5e6d30dae6272863c7d6ad9d53f7c34bd
Tiago Esdras <tiagoesdrasvieira@gmail.com>
2025-01-13 13:40:16 -0300

    add login functionality

--- ProEventos/Front/ProEventos-App/src/app/components/user/login/login.component.ts
+++ ProEventos/Front/ProEventos-App/src/app/components/user/login/login.component.ts
@@ -1,15 +1,38 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { UserLogin } from '@app/models/Identity/UserLogin';
+import { AccountService } from '@app/services/account.service';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent implements OnInit {
+  model = {} as UserLogin;
 
-  constructor() { }
+  constructor(
+    public accountService: AccountService,
+    private router: Router,
+    private toastr: ToastrService
+  ) {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  public login(): void {
+    this.accountService.login(this.model).subscribe(
+      () => {
+        this.router.navigateByUrl('/dashboard');
+      },
+      (error: any) => {
+        if (error.status == 401) {
+          this.toastr.error('Usuario ou senha inválidos');
+        }
+        else {
+          console.error(error);
+        }
+      }
+    );
   }
-
 }
