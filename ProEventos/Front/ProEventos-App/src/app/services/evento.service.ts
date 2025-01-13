ca6d0ac832a8d42c7617c422a3676b23276fd5fe
Tiago Esdras <tiagoesdrasvieira@gmail.com>
2025-01-13 13:42:04 -0300

    identação

--- ProEventos/Front/ProEventos-App/src/app/services/evento.service.ts
+++ ProEventos/Front/ProEventos-App/src/app/services/evento.service.ts
@@ -1,43 +1,42 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from '@environments/environment';
 import { Observable } from 'rxjs';
-import { take } from 'rxjs/operators'
+import { take } from 'rxjs/operators';
 import { Evento } from '../models/Evento';
 
 @Injectable()
 export class EventoService {
   baseURL = environment.apiURL + 'api/eventos';
-  constructor(private http: HttpClient) { }
+
+  constructor(private http: HttpClient) {}
 
   public getEventos(): Observable<Evento[]> {
-    return this.http.get<Evento[]>(this.baseURL)
+    return this.http.get<Evento[]>(this.baseURL).pipe(take(1));
+  }
+
+  public getEventosByTema(tema: string): Observable<Evento[]> {
+    return this.http
+      .get<Evento[]>(`${this.baseURL}/${tema}/tema`)
       .pipe(take(1));
   }
 
-  public getEventosByTema(tema : string): Observable<Evento[]> {
-    return this.http.get<Evento[]>(`${this.baseURL}/${tema}/tema`)
-      .pipe(take(1));
-  }
-
-  public getEventoById(id : number): Observable<Evento> {
-    return this.http.get<Evento>(`${this.baseURL}/${id}`)
-      .pipe(take(1));
+  public getEventoById(id: number): Observable<Evento> {
+    return this.http.get<Evento>(`${this.baseURL}/${id}`).pipe(take(1));
   }
 
   public post(evento: Evento): Observable<Evento> {
-    return this.http.post<Evento>(this.baseURL, evento)
-      .pipe(take(1));
+    return this.http.post<Evento>(this.baseURL, evento).pipe(take(1));
   }
 
   public put(evento: Evento): Observable<Evento> {
-    return this.http.put<Evento>(`${this.baseURL}/${evento.id}`, evento)
+    return this.http
+      .put<Evento>(`${this.baseURL}/${evento.id}`, evento)
       .pipe(take(1));
   }
 
-  public deleteEvento(id : number): Observable<boolean> {
-    return this.http.delete<boolean>(`${this.baseURL}/${id}`)
-      .pipe(take(1));
+  public deleteEvento(id: number): Observable<boolean> {
+    return this.http.delete<boolean>(`${this.baseURL}/${id}`).pipe(take(1));
   }
 
   postUpload(eventoId: number, file: File): Observable<Evento> {
@@ -46,7 +45,8 @@ export class EventoService {
 
     formData.append('file', fileToUpload);
 
-    return this.http.post<Evento>(`${this.baseURL}/upload-image/${eventoId}`, formData)
+    return this.http
+      .post<Evento>(`${this.baseURL}/upload-image/${eventoId}`, formData)
       .pipe(take(1));
   }
 }
