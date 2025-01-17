604099b9e55728d582ae075634c3ca31f95b0025
Tiago Esdras <tiagoesdrasvieira@gmail.com>
2025-01-17 15:55:24 -0300

    Criando PalestrantesController

---
+++ ProEventos/Back/src/ProEventos.API/Controllers/PalestrantesController.cs
@@ -0,0 +1,97 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ProEventos.API.Extensions;
+using ProEventos.Application.Contratos;
+using ProEventos.Application.Dtos;
+using ProEventos.Persistence.Models;
+using System;
+using System.Threading.Tasks;
+
+namespace ProEventos.API.Controllers
+{
+    [Authorize]
+    [ApiController]
+    [Route("api/[controller]")]
+    public class PalestrantesController : ControllerBase
+    {
+        private readonly IPalestranteService _palestranteService;
+
+        public PalestrantesController(IPalestranteService palestranteService)
+        {
+            _palestranteService = palestranteService;
+        }
+
+        [HttpGet("all")]
+        public async Task<IActionResult> GetAll([FromQuery] PageParams pageParams)
+        {
+            try
+            {
+                var palestrantes = await _palestranteService.GetAllPalestrantesAsync(pageParams, true);
+                if (palestrantes == null)
+                    return NoContent();
+
+                Response.AddPagination(palestrantes.CurrentPage,
+                                       palestrantes.PageSize,
+                                       palestrantes.TotalCount,
+                                       palestrantes.TotalPages);
+
+                return Ok(palestrantes);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, $"Erro ao tentar recuperar palestrantes. Erro: {ex.Message}");
+            }
+        }
+
+        [HttpGet()]
+        public async Task<IActionResult> GetPalestrantes()
+        {
+            try
+            {
+                var palestrante = await _palestranteService.GetPalestranteByUserIdAsync(User.GetUserId(), true);
+                if (palestrante == null)
+                    return NoContent();
+
+                return Ok(palestrante);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, $"Erro ao tentar recuperar palestrantes. Erro: {ex.Message}");
+            }
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post(PalestranteAddDto palestranteAddDto)
+        {
+            try
+            {
+                var palestrante = await _palestranteService.GetPalestranteByUserIdAsync(User.GetUserId(), false);
+                palestrante ??= await _palestranteService.AddPalestrantes(User.GetUserId(), palestranteAddDto);
+
+                return Ok(palestrante);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, $"Erro ao tentar adicionar palestrante. Erro: {ex.Message}");
+            }
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> Put(PalestranteUpdateDto palestranteUpdateDto)
+        {
+            try
+            {
+                var palestrante = await _palestranteService.UpdatePalestrante(User.GetUserId(), palestranteUpdateDto);
+                if (palestrante == null)
+                    return NoContent();
+
+                return Ok(palestrante);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, $"Erro ao tentar atualizar palestrante. Erro: {ex.Message}");
+            }
+        }
+    }
+}
